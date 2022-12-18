b06e5b27ffc50c6a11c4eb962d582627e6ceb6fd
David Turner <davidbrockturner@hotmail.com>
2022-12-18 15:10:36 -0330

    Got balance test functioning

--- __test__/balanced.test.js
+++ __test__/balanced.test.js
@@ -1,28 +1,115 @@
-const avl = require("../dataStructures/bst_avl_trees");
-const mdb = require("../services/mdb.js");
+// const avl = require("../dataStructures/bst_avl_trees");
+// const mdb = require("../services/mdb");
+// let server;
 
-test("Number of Balanced Entries Test", async () => {
-  // Connect to the MongoDB instance
-  const client = await mdb.connect();
-  const db = client.db(algor_sprint2);
-  const collection = db.collection(input);
+// describe("Connects to MongoDB", () => {
+//   beforeAll(async () => {
+//     expect(mdb.connect()).toBeTruthy();
+//   });
+//   afterAll(() => {
+//     mdb.close();
+//   });
 
-  // Find all the entries in the collection
-  const docs = await collection.find({}).toArray();
+//   test("Number of Balanced Entries Test", async () => {
+//     // Connect to the MongoDB instance
+//     const client = await mdb.connect();
+//     const db = client.db(algor_sprint2);
+//     const collection = db.collection(input);
 
-  // Create an AVL tree
-  const tree = new avl();
+//     // Find all the entries in the collection
+//     const docs = await collection.find({}).toArray();
 
-  // Insert each entry into the AVL tree
-  docs.forEach((doc) => {
-    tree.insert(doc);
+//     // Create an AVL tree
+//     const tree = new avl();
+
+//     // Insert each entry into the AVL tree
+//     docs.forEach((doc) => {
+//       tree.insert(doc);
+//     });
+
+//     // Check whether the AVL tree is balanced
+//     const isBalanced = tree.height() <= Math.log2(docs.length + 1);
+
+//     // Assert that the AVL tree is balanced
+//     expect(isBalanced).toBe(true, "AVL tree is not balanced");
+
+//     client.close();
+//   });
+// });
+
+// const avl = require("../dataStructures/bst_avl_trees");
+// const mdb = require("../services/mdb");
+// global.collection = mdb.db("algor_sprint2").collection("input");
+
+// describe("AVL tree balance test", () => {
+//   let client;
+//   let db;
+//   let collection;
+
+//   beforeAll(async () => {
+//     client = await collection.connect();
+//     db = client.db(algor_sprint2);
+//     collection = db.collection(input);
+//   });
+
+//   afterAll(() => {
+//     client.close();
+//   });
+
+//   test("Number of Balanced Entries Test", async () => {
+//     // Find all the entries in the collection
+//     const docs = await collection.find({}).toArray();
+
+//     // Create an AVL tree
+//     const tree = new avl();
+
+//     // Insert each entry into the AVL tree
+//     docs.forEach((doc) => {
+//       tree.insert(doc);
+//     });
+
+//     // Check whether the AVL tree is balanced
+//     const isBalanced = tree.height() <= Math.log2(docs.length + 1);
+
+//     // Assert that the AVL tree is balanced
+//     expect(isBalanced).toBe(true, "AVL tree is not balanced");
+//   });
+// });
+
+const { AVLTree } = require("../dataStructures/bst_avl_trees");
+const mdb = require("../services/mdb");
+let db = mdb.db("algor_sprint2").collection("input").find({});
+
+describe("AVL tree balance test", () => {
+  beforeAll(async () => {
+    await mdb.connect();
   });
 
-  // Check whether the AVL tree is balanced
-  const isBalanced = tree.height() <= Math.log2(docs.length + 1);
+  afterAll(() => {
+    mdb.close();
+  });
 
-  // Assert that the AVL tree is balanced
-  expect(isBalanced).toBe(true, "AVL tree is not balanced");
+  test("Number of Balanced Entries Test", async () => {
+    // Find all the entries in the collection
+    const docs = await db.toArray();
+    // Create an AVL tree
+    const tree = new AVLTree();
 
-  client.close();
+    // Insert each entry into the AVL tree
+    docs.forEach((doc) => {
+      tree.insert(doc);
+    });
+
+    // Check whether the AVL tree is balanced
+    const isBalanced = tree.getNodeHeight() <= Math.log2(docs.length + 1);
+
+    // Assert that the AVL tree is balanced
+    expect(isBalanced).toBe(true, "AVL tree is not balanced");
+
+    if (isBalanced) {
+      console.log("AVL trees are balanced");
+    } else {
+      console.log("AVL trees are NOT balanced");
+    }
+  });
 });
